57da401b95e86d6f17be57d19915b130708ea261
Pawel Kowalik <2063440-pawel-kow@users.noreply.gitlab.com>
2024-09-27 22:39:01 +0200

    DOCKER: move tokenizer model download to docker image build

---
+++ .docker/setup_tok_models.py
@@ -0,0 +1,15 @@
+print("INFO: loading indian lgg")
+from inltk.inltk import setup as ind_setup
+
+ind_setup("hi")
+ind_setup("pa")
+ind_setup("gu")
+ind_setup("kn")
+ind_setup("ml")
+ind_setup("mr")
+ind_setup("bn")
+ind_setup("ta")
+ind_setup("ur")
+ind_setup("ne")
+
+print("--> all done")
